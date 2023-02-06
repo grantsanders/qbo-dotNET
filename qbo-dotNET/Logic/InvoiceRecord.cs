5a41900a49f17f0705924a1086681b5c8ca1c2d6
grantsanders <grantsanders89@gmail.com>
2023-02-06 00:28:15 -0500

    added new class and todo

---
+++ qbo-dotNET/Logic/InvoiceRecord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace qbo_dotNET.Logic
+{
+    public class InvoiceRecord
+    {
+        public InvoiceRecord() { }
+
+        private string id = "";
+        private bool importedSuccessfully { get; set; }
+        private string numberOfInvoices { get; set; }
+        private string created = "";
+        private string note = "";
+
+    }
+}
+
